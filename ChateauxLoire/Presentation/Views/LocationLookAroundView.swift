a648f8011df0931c7b98043eb87cf9371429a035
Cityhunter <johan.guenaoui@gmail.com>
2024-02-08 19:47:11 +0100

    Add LookAround

---
+++ ChateauxLoire/Presentation/Views/LocationLookAroundView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationLookAroundView.swift
+//  ChateauxLoire
+//
+//  Created by Johan Guenaoui on 08/02/2024.
+//
+
+import SwiftUI
+
+struct LocationLookAroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationLookAroundView()
+}
